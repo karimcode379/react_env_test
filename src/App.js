cc10714641ab40d74d16299b048aa11ee5d19554
karimcode379 <karimcode379@gmail.com>
2022-06-15 09:31:02 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const apiKey = process.env.REACT_APP_API_KEY;
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://newsapi.org/v2/everything?q=tesla&from=2022-05-15&sortBy=publishedAt&apiKey=${apiKey}`)
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, [])
+
+  console.log(data);
+
   return (
     <div className="App">
       <header className="App-header">
